91179141f651f32c7dbacdb5ef2044cf8971986b
hayden <48267247+hayden-fr@users.noreply.github.com>
2025-05-07 16:36:34 +0800

    feat: add auto generate easycom types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import uni from '@dcloudio/vite-plugin-uni'
+import easycomTypes from 'uniapp-easycom-types'
 import autoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     uni(),
+    easycomTypes({
+      dts: 'src/types/easycom.d.ts',
+    }),
     autoImport({
       imports: ['vue', 'pinia', 'uni-app'],
       dts: 'src/types/auto-imports.d.ts',
