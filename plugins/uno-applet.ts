50b24565bfcc17c51aef67ebdaac3d28d1666ef1
hayden <48267247+hayden-fr@users.noreply.github.com>
2025-05-18 20:44:42 +0800

    fix: adapt to multi platform

--- plugins/uno-applet.ts
+++ plugins/uno-applet.ts
@@ -21,12 +21,25 @@ export default function UnoCSSApplet(): Plugin[] {
 
       generateBundle(_, bundle) {
         // 解决加入 unocss 后，uniapp 无法生成 .wxss 的问题
-        for (const chunk in bundle) {
-          if (chunk.endsWith('.css')) {
-            const wxss = chunk.replace('.css', '.wxss')
-            bundle[wxss] = bundle[chunk]
-            bundle[wxss].fileName = wxss
-            delete bundle[chunk]
+        const platform = process.env.UNI_PLATFORM
+        const specialCssSuffix: Record<string, string> = {
+          'mp-alipay': 'acss',
+          'mp-lark': 'ttss',
+          'mp-qq': 'qss',
+          'mp-toutiao': 'ttss',
+          'mp-weixin': 'wxss',
+        }
+        const specialPlatform = Object.keys(specialCssSuffix).join('|')
+        const platformRegexp = new RegExp(`(${specialPlatform})`, 'i')
+        if (platform && platformRegexp.test(platform)) {
+          for (const chunk in bundle) {
+            if (chunk.endsWith('.css')) {
+              const suffix = `.${specialCssSuffix[platform]}`
+              const cssName = chunk.replace('.css', suffix)
+              bundle[cssName] = bundle[chunk]
+              bundle[cssName].fileName = cssName
+              delete bundle[chunk]
+            }
           }
         }
       },
