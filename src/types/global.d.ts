2e6969f8fa05ab0301921d593ed336d43782b6e5
Hayden <48267247+hayden-fr@users.noreply.github.com>
2025-09-23 11:42:45 +0800

    refactor(types): 重组类型分组

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -9,30 +9,6 @@ declare global {
 
 declare global {
   /**
-   * 日志接口
-   */
-  interface LoggingInterface {
-    info: (...args: any[]) => void
-    warn: (...args: any[]) => void
-    error: (...args: any[]) => void
-    debug: (...args: any[]) => void
-    log: (...args: any[]) => void
-  }
-
-  // prettier-ignore
-  type ClassNameValue = false | null | undefined | string | Record<string, boolean> | Array<ClassNameValue>;
-
-  /**
-   * 语义化对象
-   */
-  type Semantic<S, V> = Partial<Record<S, V>>
-
-  /**
-   * 必填语义化对象
-   */
-  type RequiredSemantic<S, V> = Record<S, V>
-
-  /**
    * 响应结构
    */
   interface ResponseStructure<T> {
@@ -59,4 +35,30 @@ declare global {
     size: number
     total: number
   }
+}
+
+declare global {
+  /**
+   * 日志接口
+   */
+  interface LoggingInterface {
+    info: (...args: any[]) => void
+    warn: (...args: any[]) => void
+    error: (...args: any[]) => void
+    debug: (...args: any[]) => void
+    log: (...args: any[]) => void
+  }
+
+  // prettier-ignore
+  type ClassNameValue = false | null | undefined | string | Record<string, boolean> | Array<ClassNameValue>;
+
+  /**
+   * 语义化对象
+   */
+  type Semantic<S, V> = Partial<Record<S, V>>
+
+  /**
+   * 必填语义化对象
+   */
+  type RequiredSemantic<S, V> = Record<S, V>
 }
