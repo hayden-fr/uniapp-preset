da536559bd745cdb86529d89e67f1fed38f06233
Hayden <48267247+hayden-fr@users.noreply.github.com>
2025-09-29 11:40:28 +0800

    fix: 解决微信小程序中循环引用的问题

--- src/utils/plugins.ts
+++ src/utils/plugins.ts
@@ -1,12 +1,23 @@
-import { getUniCacheInstance } from '@/plugins/cache'
-import { getUniLoggingInstance } from '@/plugins/logging'
-import { getUniHttpRequestInstance } from '@/plugins/request'
+import * as plugins_auth from '@/plugins/auth'
+import * as plugins_cache from '@/plugins/cache'
+import * as plugins_init from '@/plugins/init'
+import * as plugins_logging from '@/plugins/logging'
+import * as plugins_request from '@/plugins/request'
+import * as plugins_router from '@/plugins/router'
+import * as plugins_theme from '@/plugins/theme'
 
-export { useAuth } from '@/plugins/auth'
-export { useInit } from '@/plugins/init'
-export { useRoute } from '@/plugins/router'
-export { useTheme } from '@/plugins/theme'
-
-export const cache = getUniCacheInstance()
-export const logging = getUniLoggingInstance()
-export const request = getUniHttpRequestInstance()
+export const useAuth: typeof plugins_auth.useAuth = () => {
+  return plugins_auth.useAuth()
+}
+export const useInit: typeof plugins_init.useInit = () => {
+  return plugins_init.useInit()
+}
+export const useRoute: typeof plugins_router.useRoute = () => {
+  return plugins_router.useRoute()
+}
+export const useTheme: typeof plugins_theme.useTheme = () => {
+  return plugins_theme.useTheme()
+}
+export const cache = plugins_cache.getUniCacheInstance()
+export const logging = plugins_logging.getUniLoggingInstance()
+export const request = plugins_request.getUniHttpRequestInstance()
